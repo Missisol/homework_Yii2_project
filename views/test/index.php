c269b1b533b4fccb27c960e16f8c0f52c5271bc9
missisol <missisololol@gmail.com>
2019-01-11 16:18:44 +0300

    View test/index.php added.

---
+++ views/test/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @var array $product The test variable
+ * @var $model \app\models\Product
+ */
+
+?>
+
+  <div>
+    <h3>This is my test page</h3>
+    <hr>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+      dolore magna aliqua.
+    </p>
+    <hr>
+    <p>
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat.
+    </p>
+  </div>
+  <hr>
+<?= \yii\widgets\DetailView::widget(['model' => $model]) ?>
+<hr>
+<?php foreach ($product as $key => $item) : ?>
+  <div><?=$key ?>: <?= $item ?></div>
+<?php endforeach; ?>
+
